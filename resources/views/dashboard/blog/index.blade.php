e34ccfb182dca3a9832291a2d9d1ff9b7ac16047
agastyra <rangga.agastya711@gmail.com>
2022-02-19 14:09:19 +0700

    menambah tombol create blog

--- resources/views/dashboard/blog/index.blade.php
+++ resources/views/dashboard/blog/index.blade.php
@@ -6,6 +6,7 @@
     </div>
 
     <div class="table-responsive col-lg-8">
+        <a href="/dashboard/blogs/create" class="btn btn-primary mb-3">Create new blog</a>
         <table class="table table-striped table-sm">
             <thead>
                 <tr>
