6e610c501882c7e543395fc86cf90e5d7db24363
agastyra <rangga.agastya711@gmail.com>
2022-02-19 14:09:47 +0700

    view create blog

---
+++ resources/views/dashboard/blog/create.blade.php
@@ -0,0 +1,35 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Create new blog</h1>
+    </div>
+
+    <div class="col-lg-8 mb-5">
+        <form action="/dashboard/blogs" method="POST">
+            @csrf
+            <div class="mb-3">
+                <label for="title" class="form-label">Title</label>
+                <input type="input" class="form-control" id="title" name="title">
+            </div>
+            <div class="mb-3">
+                <label for="slug" class="form-label">Slug</label>
+                <input type="input" class="form-control" id="slug" name="slug" readonly>
+            </div>
+            <div class="mb-3">
+                <label for="category_id" class="form-label">Category</label>
+                <select name="category_id" id="category_id" class="form-select">
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="body" class="form-label">Body</label>
+                <input id="body" type="hidden" name="body">
+                <trix-editor input="body"></trix-editor>
+            </div>
+            <button type="submit" class="btn btn-primary">Create blog</button>
+        </form>
+    </div>
+@endsection
